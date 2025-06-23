f797d76d9ffdbf7551b8e873695daa40cd9a7abc
Yaiphaba-Ng <ng.yaiphaba@gmail.com>
2025-06-23 13:28:52 +0530

    Remove GPU flag from training script execution

--- run_training.sh
+++ run_training.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install --upgrade --quiet pip
 pip install --quiet -r requirements.txt
-nohup python whisper_finetune_script.py -g 1 > train.log 2>&1 &
+nohup python whisper_finetune_script.py > train.log 2>&1 &
