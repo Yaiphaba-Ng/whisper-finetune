9d5e73c3b80966e56a1b631bd96e4facd64709ee
Yaiphaba-Ng <ng.yaiphaba@gmail.com>
2025-06-26 17:52:24 +0530

    Update run_logger.sh to remove logfile redirection and specify output path with timestamp

--- run_logger.sh
+++ run_logger.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Run system_metrics_logger.py in the background, detached
 
-LOGFILE="system_metrics_logger.out"
 PYTHON_SCRIPT="system_metrics_logger.py"
 
-nohup python "$PYTHON_SCRIPT" > "$LOGFILE" 2>&1 &
-echo "System metrics logger started in background (PID $!). Output: $LOGFILE"
+nohup python "$PYTHON_SCRIPT" &
+echo "System metrics logger started in background (PID $!). Output: ./system_metrics/<timestamp>.txt"
